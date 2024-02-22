8aa310b4c561391d8c9f36b78c66f11e698d76ee
Mich-Kol <mich.kolackovska@gmail.com>
2024-02-22 21:47:08 +0100

    first commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+ 
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+/*const event = ref ({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'cat adoption day',
+  description:'Find a feline',
+  location:'Meow',
+  date:'january 28, 2022',
+  petsAllowed: true,
+  organizer:'Kat',
+})*/
+</script>
+
+<template>
+  <div class="event-card">
+  <h2>{{ event.title }}</h2>
+  <span>@{{ event.time }} on {{ event.date }}</span>
+
+  </div>
+ 
+   
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+/*.event-link {
+  color: #2c3e50;
+  text-decoration: none;
+}*/
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,52 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+
+const events = ref([
+  {
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'cat adoption day',
+  description:'Find a feline',
+  location:'Meow',
+  date:'january 28, 2022',
+  petsAllowed: true,
+  organizer:'Kat',
+},
+{
+id: 4582797,
+  category: 'food',
+  title: 'gardening',
+  description:'plants',
+  location:'flora city',
+  date:'march 28, 2022',
+  petsAllowed: true,
+  organizer:'Fern',
+},
+{
+  id: 7848541,
+  category: 'sustainability',
+  title: 'beach',
+  description:'Trash',
+  location:'Kokos',
+  date:'july 28, 2022',
+  petsAllowed: false,
+  organizer:'Whale',
+},
+])
+
+</script>
+
+<template>
+  <h1>Events for good</h1>
+<div class="events">
+  <EventCard v-for="event in events" :key="event.id" :event="event"/>
+</div>
+</template>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
